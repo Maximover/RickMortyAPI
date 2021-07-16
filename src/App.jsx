8e5eed909664f4d80799a00d8c71d4ea30b6d52a
Maximover <emwp.dex@poczta.onet.pl>
2021-07-16 15:43:13 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path="/" exact>
           <Home />
